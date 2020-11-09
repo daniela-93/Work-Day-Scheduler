272a38da61d49023a461105bac7a7d81f54da4dc
daniela <daniela.acuna93@gmail.com>
2020-11-08 23:30:11 -0600

    edit js

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,3 +1,16 @@
 
-     var a = moment().format("MMM Do YYYY")
-     document.getElementById("todaysdate").innerHTML = a;
+     var currentDate = moment().format("MMM Do YYYY")
+     document.getElementById("todaysdate").innerHTML = currentDate;
+
+     var currentTime = moment().format('LT');
+
+     var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+console.log(rightNow);
+
+
+
+generateStartPage();
+
+
+
+   
